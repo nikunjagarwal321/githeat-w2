2837dc106056b611f0b16a88c8ce57acc0c3b92f
Nikunj Agarwal <nikunjagaral321@gmail.com>
2018-06-15 23:46:03 -0700

    Tried solving first issue and other problems

--- README.md
+++ README.md
@@ -12,8 +12,5 @@ user's computer.
 ## What the project does
 Initially, one has to enter the directory name along with its full path. It will show all the sub-directories
 and files and the space occupied by them(in bytes) in a pie-chart. Then, he/she can navigate into its parent 
-or child directoryas per his/her wish or can delete a file or sub-directory as per his choice. It should be 
-noted that while deleting a file, one must also enter its extension alongwith the filename. If one wants to 
-navigate into a child directory, he/she has to enter the directory name which can be selected from the list 
-of directory names which is written in the pie-chart. It should be taken care that correct filename/sub-directory 
-is entered or else "FileNotFound" error will be displayed. 
+or child directory as per his/her wish or can delete a file or sub-directory as per his choice. While performing
+these two operations, one has to enter an index(key) of the file from the previously printed list.
--- githeat-w2.py
+++ githeat-w2.py
@@ -3,9 +3,9 @@ from plotly.offline import  plot, iplot
 import plotly.graph_objs as go
 import shutil
 
-
+output_dictionary = {}
 #Get size of a directory
-def dir_size(curr_path):
+def get_dir_size(curr_path):
     total_size = 0
     for dirpath, dirnames, filenames in os.walk(curr_path):
         for f in filenames:
@@ -14,50 +14,67 @@ def dir_size(curr_path):
     return total_size
 
 #Get size of all sub-directories and files
-def all_sub_size(parent_path):
+def get_all_sub_size(parent_path):
+    i = 0
     dir = os.listdir(parent_path)
     size = []
-    i = 0
+   
     for file in dir:
-        curr_path = parent_path + "\\" + dir[i]
+        curr_path = os.path.join(parent_path, file)
         if os.path.isdir(curr_path):
-            size.append(dir_size(curr_path))
+            temp_size = get_dir_size(curr_path)
+            size.append(temp_size)
         else:
-            size.append(os.path.getsize(curr_path))
-        i +=1 
+            temp_size = os.path.getsize(curr_path)
+            size.append(temp_size)
+        output_dictionary[i] = file
+        i = i + 1
+    plot_pie_chart(dir,size)
+
+#Plot the chart
+def plot_pie_chart(dir,size):   
     trace = go.Pie(labels=dir, values=size)
     plot([trace], filename='pie_chart.html')
-    print (dir)
-    print (size)  
+    i = 0
+    for file in dir:
+        print (str(i) + "." + (output_dictionary[i]) + " - " + str(size[i]) +" bytes")
+        i = i + 1
 
 
-choice = 1
-print ("Enter full path and directory name")
-input_path = input()
-all_sub_size(input_path)
-while choice:
-    print("Enter  \n0.To end \n1.To navigate to parent folder \n2.To navigate to child folder \n3.To remove a directory or file from the present directory")
-    choice = int(input())
-    if choice == 0:
-        break
-    elif choice == 1:
-        input_path = os.path.dirname(input_path)
-        all_sub_size(input_path)
-    elif choice == 2:
-        print("Enter name of child directory")
-        child_dir = input()
-        input_path = input_path + "\\" + child_dir
-        all_sub_size(input_path)
-    elif choice == 3:
-        print("Enter filename with extension or sub-directory to be deleted")
-        file_del = input()
-        if os.path.isdir(input_path + "\\" + file_del):
-            shutil.rmtree((input_path + "\\" + file_del))
-        else:
-            os.remove(input_path + "\\" + file_del) 
-        all_sub_size(input_path)
-    else:
-        print("Wrong choice")
+def main():
+    choice = 1
+    print ("Enter full path and directory name")
+    input_path = input()
+    if input_path =='.':
+        input_path = os.path.realpath('.')
+        get_all_sub_size(input_path)
+        while choice:
+            print("Enter  \n0.To end \n1.To navigate to parent folder \n2.To navigate to child folder \n3.To remove a directory or file from the present directory")
+            choice = int(input())
+            if choice == 0:
+                break
+            elif choice == 1:
+                input_path = os.path.dirname(input_path)
+                get_all_sub_size(input_path)
+            elif choice == 2:
+                print("Enter index of child directory")
+                child_dir_key = int(input())
+                child_dir = output_dictionary[child_dir_key]
+                input_path = os.path.join(input_path, child_dir)
+                get_all_sub_size(input_path)
+            elif choice == 3:
+                print("Enter index of filename or sub-directory to be deleted")
+                file_del_key = int(input())
+                file_del = output_dictionary[file_del_key]
+                if os.path.isdir(os.path.join(input_path, file_del)):
+                    shutil.rmtree(os.path.join(input_path, file_del))
+                else:
+                    os.remove(os.path.join(input_path, file_del))
+                get_all_sub_size(input_path)
+            else:
+                print("Wrong choice")
 
+if __name__== "__main__":
+    main()
 
 
